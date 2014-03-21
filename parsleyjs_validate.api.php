0e1adbc463bacd403680e38613250ce0f4851e28
Muhammad Reda <say.hello@mreda.net>
2014-03-21 18:56:44 +0200

    Initial Commit

---
+++ parsleyjs_validate.api.php
@@ -0,0 +1,12 @@
+<?php // $Id$
+
+
+
+function hook_parsleyjs_validate_widgets_and_targets()
+{
+    return array(
+        'text_textfield'    => 'value',
+        'number'            => 'value',
+    );
+}
+
